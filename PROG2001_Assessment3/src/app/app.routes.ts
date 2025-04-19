cfd0e88b776d358af56320c981d57a367a1c2e16
lyue11 <l.yue.11@student.scu.edu.au>
2025-04-19 11:14:50 +0800

    修改页面

--- PROG2001_Assessment3/src/app/app.routes.ts
+++ PROG2001_Assessment3/src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   {
     path: 'tabs',
     loadComponent: () => import('./tabs/tabs.page').then(m => m.TabsPage),
     children: [
-      { path: 'tab1', loadComponent: () => import('./tab1/tab1.page').then(m => m.Tab1Page) },
-      { path: 'tab2', loadComponent: () => import('./tab2/tab2.page').then(m => m.Tab2Page) },
-      { path: 'tab3', loadComponent: () => import('./tab3/tab3.page').then(m => m.Tab3Page) },
-      // 把你的菜单和场景页作为子路由
-      { path: 'menu', loadComponent: () => import('./menu/menu.page').then(m => m.MenuPage) },
-      { path: 'scene1', loadComponent: () => import('./scene1/scene1.page').then(m => m.Scene1Page) }
-    ]
-  }
+      {
+        path: 'item-list',  // 列表页（对应 Tab1）
+        loadComponent: () => import('./pages/item-list/item-list.page').then(m => m.ItemListPage),
+      },
+      {
+        path: 'add-item',   // 添加页（对应 Tab2）
+        loadComponent: () => import('./pages/add-item/add-item.page').then(m => m.AddItemPage),
+      },
+      {
+        path: 'privacy',    // 隐私页（对应 Tab3）
+        loadComponent: () => import('./pages/privacy/privacy.page').then(m => m.PrivacyPage),
+      },
+      { path: '', redirectTo: 'tabs/item-list', pathMatch: 'full' },
+    ],
+  },
+  { path: '', redirectTo: 'tabs/item-list', pathMatch: 'full' },
 ];
